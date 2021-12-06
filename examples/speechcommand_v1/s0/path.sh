1240136ebaec1d94bb42da6e31e08d9c090dfabe
jingyong hou <houjingyong@gmail.com>
2021-12-06 11:52:49 +0800

    updata path.sh

--- examples/speechcommand_v1/s0/path.sh
+++ examples/speechcommand_v1/s0/path.sh
@@ -1,1 +1,5 @@
-../../hi_xiaowen/s0/path.sh
+export PATH=$PWD:$PATH
+
+# NOTE(kan-bayashi): Use UTF-8 in Python to avoid UnicodeDecodeError when LC_ALL=C
+export PYTHONIOENCODING=UTF-8
+export PYTHONPATH=../../../:$PYTHONPATH
