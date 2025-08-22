0eeec750159c1264a74a7679b2551a6a31a44e92
Menglong Xu <32296227+mlxu995@users.noreply.github.com>
2025-08-22 14:40:47 +0800

    [fix] make fsmn compatible with wenet's datapipe (#202)
    * [fix] make fsmn compatible with wenet's datapipe
    
    * fix lint
    
    ---------
    
    Co-authored-by: menglong.xu <menglong.xu@aispeech.com>

--- wekws/dataset/init_dataset.py
+++ wekws/dataset/init_dataset.py
@@ -15,8 +15,57 @@
 # limitations under the License.
 
 import copy
+import torch
+
+from functools import partial
 from wenet.dataset.dataset import Dataset
 
+
+def context_expansion(sample, left=1, right=1):
+    """ expand left and right frames
+        Args:
+            sample: {key, feats, feats_lengths, ...}
+            left (int): feature left context frames
+            right (int): feature right context frames
+
+        Returns:
+            sample: {key, feats, feats_lengths, ...}
+    """
+    feats = sample['feats']  # (B, T, D)
+    batch_size = feats.shape[0]
+    ctx_frm = feats.shape[1]
+    ctx_dim = feats.shape[2] * (left + right + 1)
+    feats_ctx = torch.zeros(batch_size, ctx_frm, ctx_dim, dtype=torch.float32)
+    index = 0
+    for lag in range(-left, right + 1):
+        feats_ctx[:, :, index:index + feats.shape[2]] = torch.roll(
+            feats, -lag, 1)
+        index = index + feats.shape[2]
+    # replication pad left margin
+    for idx in range(left):
+        for cpx in range(left - idx):
+            feats_ctx[:, idx, cpx * feats.shape[2]:(cpx + 1) * feats.shape[2]] = \
+                feats_ctx[:, left, :feats.shape[2]]
+
+    feats_ctx = feats_ctx[:, :feats_ctx.shape[1] - right]
+    sample['feats'] = feats_ctx  # (B, T, D * n) where n = left + right + 1
+    sample['feats_lengths'] = sample['feats_lengths'] - right
+    return sample
+
+def frame_skip(sample, skip_rate=1):
+    """ skip frame
+        Args:
+            sample: {key, feats, feats_lengths, ...}
+            skip_rate (int): take every N-frames for model input
+
+        Returns:
+            sample: {key, feats, feats_lengths, ...}
+    """
+    feats_skip = sample['feats'][:, ::skip_rate, :]
+    sample['feats'] = feats_skip
+    feats_lengths = torch.ceil(sample['feats_lengths'] / skip_rate)
+    sample['feats_lengths'] = feats_lengths.to(dtype=torch.int16)
+    return sample
 
 def init_dataset(dataset_type: str = 'asr',
                  data_type: str = 'raw',
@@ -40,4 +89,13 @@ def init_dataset(dataset_type: str = 'asr',
         cv_conf['list_shuffle'] = False
         conf = cv_conf
 
-    return Dataset(data_type, data_list_file, tokenizer, conf, partition)
+    dataset = Dataset(data_type, data_list_file, tokenizer, conf, partition)
+
+    if conf.get('context_expansion', False):
+        dataset = dataset.map(
+            partial(context_expansion, **conf.get('context_expansion_conf', {})))
+
+    if conf.get('frame_skip', 1) > 1:
+        dataset = dataset.map(partial(frame_skip, skip_rate=conf.get('frame_skip')))
+
+    return dataset
